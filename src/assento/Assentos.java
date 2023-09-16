244f2313dc38e48483a5349afe0283aba0753706
Giovani <giovanifetzner01@gmail.com>
2023-09-16 15:30:25 -0300

    Delegação de códigos envolvendo métodos repetitivos para a classe Assentos.java

--- src/assento/Assentos.java
+++ src/assento/Assentos.java
@@ -4,6 +4,8 @@ public class Assentos {
 
     String[][] assentos = new String[12][14];
     final String ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    final String ERRO = "E R R O !! %nInforme valores válidos, entre A e " +
+            "L para linhas e 1 e 14 para colunas %n";
 
     public String cadastrarReserva(String linha, int coluna){
         String linhaUpperCase = linha.toUpperCase();
@@ -12,8 +14,7 @@ public class Assentos {
         if(confereEntradaDeDados(coluna, linhaUpperCase)){
             return "Reservado";
         } else {
-            return "E R R O !! %nInforme valores válidos, entre A e " +
-                    "L para linhas e 1 e 14 para colunas %n";
+            return ERRO;
         }
     }
 
@@ -24,8 +25,7 @@ public class Assentos {
         if(confereEntradaDeDados(coluna, linhaUpperCase)){
             return null;
         } else{
-            return "E R R O !! %nInforme valores válidos, entre A e " +
-                    "L para linhas e 1 e 14 para colunas %n";
+            return ERRO;
         }
 
     }
