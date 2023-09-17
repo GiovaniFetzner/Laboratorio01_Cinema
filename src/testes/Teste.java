d6d7ef7ec51e0d838bcee0e20349aa1beec7edb0
Renan Milech Pereira <renanmp@outlook.com>
2023-09-17 17:48:56 -0300

    Teste de menu, bug só na analise de dados

--- src/testes/Teste.java
+++ src/testes/Teste.java
@@ -10,50 +10,62 @@ public class Teste {
         Mapa mapaDeCadeiras = new Mapa(assentos);
         String linha;
         int coluna;
+        int escolha;
+        boolean sair = true;
 
         Scanner leitura = new Scanner(System.in);
+        Scanner leitura2 = new Scanner(System.in);
 
-        /*String [][] provisorio = assentos.getAssentos();
+        System.out.println("Bem vindo ao cinema dos Guri!!!");
 
-        for (int i = 0; i < provisorio.length; i++) {
-            System.out.print(assentos.getCharAtAlfabeto(i) + " ");
-            for (int j = 0; j < provisorio[0].length; j++) {
-                if (provisorio[i][j] != null) System.out.print(provisorio[i][j] + " ");
-                else {
-                    provisorio[i][j] = Integer.toString(j+1);
-                    System.out.print(provisorio[i][j] + " ");
-                }
+        while (sair) {
+
+            System.out.println("\nFaça sua escolha:");
+            System.out.println("1. Fazer Reserva\n2. Cancelar Reserva\n3. Analise de assentos\n4. Ver Mapa\n" +
+                    "5. Sair da Operação");
+            escolha = leitura2.nextInt();
+
+            if (escolha == 1) {
+                mapaDeCadeiras.mostrarMapa(assentos);
+                System.out.print("Informe a linha que você deseja: ");
+                linha = leitura.next();
+                System.out.printf("%nInforme a coluna que você deseja :");
+                coluna = leitura.nextInt();
+                System.out.println(assentos.cadastrarReserva(linha, coluna));
+
             }
-            System.out.println();
-        }*/
-        mapaDeCadeiras.mostrarMapa(assentos);
-        System.out.print("Informe a linha que você deseja fazer a reserva: ");
-        linha = leitura.next();
-        System.out.printf("%nInforme a coluna que você deseja fazer a reserva:");
-        coluna = leitura.nextInt();
-        System.out.println(assentos.cadastrarReserva(linha,coluna));
-        mapaDeCadeiras.mostrarMapa(assentos);
 
-        System.out.print("Quantidade de reservas: ");
-        System.out.println(assentos.getQuantidadeDeReservas());
-        System.out.print("Quantidade de poltronas livres: ");
-        System.out.println(assentos.getQuantidadeDePoltronasLivres());
-        System.out.print("Quantidade de total: ");
-        System.out.println(assentos.getQUANTIDADE_TOTAL_POLTRONAS());
+            else if (escolha == 2){
+                mapaDeCadeiras.mostrarMapa(assentos);
+                System.out.print("Informe a linha do assento que você quer cancelar: ");
+                linha = leitura.next();
+                System.out.printf("%nInforme a coluna do assento que você quer cancelar:");
+                coluna = leitura.nextInt();
+                System.out.println(assentos.cancelarReserva(linha, coluna));
+            }
 
-        System.out.print("Informe a linha do assento que você quer cancelar: ");
-        linha = leitura.next();
-        System.out.printf("%nInforme a coluna do assento que você quer cancelar:");
-        coluna = leitura.nextInt();
-        System.out.println(assentos.cancelarReserva(linha,coluna));
-        mapaDeCadeiras.mostrarMapa(assentos);
+            else if (escolha == 3){
+                System.out.print("Quantidade de total: ");
+                System.out.println(assentos.getQUANTIDADE_TOTAL_POLTRONAS());
+                System.out.print("Quantidade de reservas: ");
+                System.out.println(assentos.getQuantidadeDeReservas());
+                System.out.print("Quantidade de poltronas livres: ");
+                System.out.println(assentos.getQuantidadeDePoltronasLivres());
+            }
 
-        System.out.print("Quantidade de reservas: ");
-        System.out.println(assentos.getQuantidadeDeReservas());
-        System.out.print("Quantidade de poltronas livres: ");
-        System.out.println(assentos.getQuantidadeDePoltronasLivres());
-        System.out.print("Quantidade de total: ");
-        System.out.println(assentos.getQUANTIDADE_TOTAL_POLTRONAS());
+            else if (escolha == 4){
+                mapaDeCadeiras.mostrarMapa(assentos);
+            }
+
+            else if (escolha == 5){
+                System.out.println("Operação Finalizada");
+
+                sair = false;
+            }
+            else {
+                System.out.println("Nenhuma escolha valida\nTente novamente\n");
+            }
+        }
 
     }
 }
